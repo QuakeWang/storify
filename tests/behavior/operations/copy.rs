0ecc86d69882f8c9c074a9b9694f5cd67b5e7502
liwuhen <liwuhen5788@gmail.com>
2025-08-24 16:35:30 +0800

    feat: fix cp test
    Signed-off-by: liwuhen <liwuhen5788@gmail.com>

--- tests/behavior/operations/copy.rs
+++ tests/behavior/operations/copy.rs
@@ -8,34 +8,64 @@ use std::path::Path;
 pub fn tests(client: &StorageClient, tests: &mut Vec<Trial>) {
     tests.extend(async_trials!(
         client,
-        test_copy_same_directory,
+        test_copy_file_to_existing_directory,
+        test_copy_file_to_new_path,
         test_copy_across_directory,
         test_copy_overwrite_existing_file,
+        test_copy_to_nonexistent_directory,
         test_copy_non_existent_file
     ));
 }
 
-async fn test_copy_same_directory(client: StorageClient) -> Result<()> {
+async fn test_copy_file_to_existing_directory(client: StorageClient) -> Result<()> {
+    let src_dir = TEST_FIXTURE.new_dir_path();
+    client.operator().create_dir(&src_dir).await?;
+
     let (src_file, content, _) = TEST_FIXTURE.new_file(client.operator());
-
-    let src_path = TEST_FIXTURE.new_dir_path();
-    client.operator().create_dir(&src_path).await?;
-
-    let src_file_path = format!("{}{}", src_path, src_file);
+    let src_file_path = format!("{}{}", src_dir, src_file);
     client
         .operator()
         .write(&src_file_path, content.clone())
         .await?;
 
+    let dest_dir = TEST_FIXTURE.new_dir_path();
+    client.operator().create_dir(&dest_dir).await?;
+
     ossify_cmd()
         .arg("cp")
         .arg(&src_file_path)
-        .arg(&src_path)
+        .arg(&dest_dir)
         .assert()
         .success();
 
-    let dst_content = client.operator().read(&src_file_path).await?;
+    let dest_file_path = format!("{}{}", dest_dir, src_file);
+    let dst_content = client.operator().read(&dest_file_path).await?;
     assert_eq!(content, dst_content.to_vec());
+
+    let src_content = client.operator().read(&src_file_path).await?;
+    assert_eq!(content, src_content.to_vec());
+
+    Ok(())
+}
+
+async fn test_copy_file_to_new_path(client: StorageClient) -> Result<()> {
+    let (src_file, content, _) = TEST_FIXTURE.new_file(client.operator());
+    client.operator().write(&src_file, content.clone()).await?;
+
+    let dest_file = TEST_FIXTURE.new_file_path();
+
+    ossify_cmd()
+        .arg("cp")
+        .arg(&src_file)
+        .arg(&dest_file)
+        .assert()
+        .success();
+
+    let dst_content = client.operator().read(&dest_file).await?;
+    assert_eq!(content, dst_content.to_vec());
+
+    let src_content = client.operator().read(&src_file).await?;
+    assert_eq!(content, src_content.to_vec());
 
     Ok(())
 }
@@ -50,6 +80,7 @@ async fn test_copy_across_directory(client: StorageClient) -> Result<()> {
         "{}",
         Path::new(&src_path).file_name().unwrap().to_string_lossy()
     );
+
     let final_dest_path = join_remote_path(&dest_path, &final_dest_path);
 
     ossify_cmd()
@@ -59,8 +90,8 @@ async fn test_copy_across_directory(client: StorageClient) -> Result<()> {
         .assert()
         .success();
 
-    let dst_content = client.operator().read(&final_dest_path).await?;
-    assert_eq!(content, dst_content.to_vec());
+    let dest_content = client.operator().read(&final_dest_path).await?;
+    assert_eq!(content, dest_content.to_vec());
 
     let src_content = client.operator().read(&src_path).await?;
     assert_eq!(content, src_content.to_vec());
@@ -98,6 +129,23 @@ async fn test_copy_overwrite_existing_file(client: StorageClient) -> Result<()> 
 
     let src_content_after = client.operator().read(&src_file_path).await?;
     assert_eq!(src_content, src_content_after.to_vec());
+
+    Ok(())
+}
+
+async fn test_copy_to_nonexistent_directory(client: StorageClient) -> Result<()> {
+    let (src_file, content, _) = TEST_FIXTURE.new_file(client.operator());
+    client.operator().write(&src_file, content.clone()).await?;
+
+    let nonexistent_dir = format!("{}/", TEST_FIXTURE.new_dir_path());
+
+    ossify_cmd()
+        .arg("cp")
+        .arg(&src_file)
+        .arg(&nonexistent_dir)
+        .assert()
+        .failure()
+        .stderr(predicate::str::contains("Invalid path"));
 
     Ok(())
 }
